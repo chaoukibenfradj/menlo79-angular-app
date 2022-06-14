557f4699d2b1ffc8a26eed9a531c0c3f65880cde
Chaouki Ben Fradj <chaoukibenfraj@gmail.com>
2022-06-14 11:46:43 +0200

    add: Book model

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('menlo79-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('menlo79-app app is running!');
-  });
 });
