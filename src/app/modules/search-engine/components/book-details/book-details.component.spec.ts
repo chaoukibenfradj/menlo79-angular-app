0d962bc0843c435a40edac1e0c1e4084f826a593
Chaouki Ben Fradj <chaoukibenfraj@gmail.com>
2022-06-14 12:00:41 +0200

    add: books details page

---
+++ src/app/modules/search-engine/components/book-details/book-details.component.spec.ts
@@ -0,0 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { BooksService } from '../../services/books.service';
+
+import { BookDetailsComponent } from './book-details.component';
+
+describe('BookDetailsComponent', () => {
+  let component: BookDetailsComponent;
+  let fixture: ComponentFixture<BookDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BookDetailsComponent],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule
+      ],
+      providers: [BooksService,
+        { provide: ActivatedRoute, useValue: {paramMap: of({isbn: "1933988673"})} },
+      ],
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(BookDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/search-engine/components/book-details/book-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs';
+import { BooksService } from '../../services/books.service';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  bookDetails$ = this.activatedRoute.paramMap
+  .pipe(
+    map(params=>params.get('isbn'))
+  )
+  .pipe(
+    switchMap((isbn) => this.booksService.getBookDetails(isbn??''))
+  );
+
+  constructor(private activatedRoute: ActivatedRoute, private booksService: BooksService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(val=>console.log(val));
+  }
+
+}
